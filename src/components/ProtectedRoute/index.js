83611284190148ade902f2f97fa9338211cd53cd
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2023-05-12 07:55:15 +0000

    1-tstcase failed

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,12 @@
+import {Redirect, Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const JwtToken = Cookies.get('jwt_token')
+  if (JwtToken === undefined) {
+    return <Redirect to="/ebank/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
